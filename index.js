eebbf1a71564e645495cf6433b8e5c0a0b65eb5e
AnthonyKuehl <anthony.kuehl.sanitz@outlook.de>
2023-01-20 17:19:00 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+const port = process.env.PORT || 3000;
+let bodyParser = require('body-parser');
+let helmet = require('helmet');
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+const app = express();
+app.use(cors())
+app.use(express.json());
+app.set("view engine", "ejs");
+app.use(helmet());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/css', express.static('css'))
+
+const routes = require('./routes/routes.js');
+const userLogin = require('./routes/userLogin.js')
+
+app.use('/', userLogin)
+app.use('/api', routes)
+
+app.listen(port, () => {
+    console.log(`Server Started at ${port}`)
+})
---
+++ routes/userLogin.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const User = require('../models/user');
+const router = express.Router();
+var async = require('async');
+
+router.get('', async (req, res) => {
+    res.render('login', {});
+})
+
+router.post('/login', async (req, res) => {
+    async.waterfall([
+        function (callback) {
+          User.findOne({
+            name: req.body.email
+          }, function (err, result) {
+            if (err) {
+              console.log(err);
+              res.send({error: "An error has occurred"});
+            } else {
+              if (result == null) {
+                res.send({"error": "This email address is not recognised, please check you have entered your email correctly"});
+              } else {
+                console.log("Email recognised");
+                callback(null, result);
+              }
+            }
+          });
+        },
+        async function (result, callback){
+          var password = result.password;
+          if (req.body.password !== password){
+            res.send({"error":"Sorry your password is incorrect"});
+          } else {
+            res.redirect('/api')
+            await User.findOneAndUpdate({name: req.body.email}, {loggedIn: true}, {new : true})
+          }
+        }
+      ])
+})
+
+router.post('/logout', async (req, res) => {
+  await User.findOneAndUpdate({name: req.body.email}, {loggedIn: false}, {new : true})
+  res.redirect('/')
+})
+
+module.exports = router;
