836df4d2f0229be8eeebe75ed3390fa0ab5eb2d0
AnthonyKuehl <anthony.kuehl.sanitz@outlook.de>
2023-02-22 14:57:18 +0100

    LEV MV

--- routes/u15routes.js
+++ routes/u15routes.js
@@ -258,14 +258,14 @@ router.get('/toggleLive/:id/:home/:away/:live', async (req, res) => {
 
         if(live1 == false) {
             // home Update
-            const goalsGameHome = await Goal.find({ verein: req.params.home, liga: liga }).length;
+            const goalsGameHome = await Goal.find({ verein: req.params.home, liga: liga });
             const ggoalsHome = await Goal.find({ gameID: req.params.id , verein: req.params.away, liga: liga });
             const ggoalsHome1 = await Tabelle.findOne({ name: req.params.home, liga: liga });
-            await Tabelle.findOneAndUpdate({name: req.params.home, liga: liga}, {goals: goalsGameHome, ggoals: ggoalsHome1.get('ggoals') + ggoalsHome.length, games: ggoalsHome1.get('games') + 1}, options);
+            await Tabelle.findOneAndUpdate({name: req.params.home, liga: liga}, {goals: goalsGameHome.length, ggoals: ggoalsHome1.get('ggoals') + ggoalsHome.length, games: ggoalsHome1.get('games') + 1}, options);
 
             // away Update
             const goalsGameAway = await Goal.find({ verein: req.params.away, liga: liga });
-            const ggoalsAway = await Goal.find({ gameID: req.params.id , verein: req.params.home, liga: liga });
+            const ggoalsAway = await Goal.find({ gameID: req.params.id , verein: req.params.away, liga: liga });
             const ggoalsAway1 = await Tabelle.findOne({ name: req.params.away, liga: liga });
             await Tabelle.findOneAndUpdate({name: req.params.away, liga: liga}, {goals: goalsGameAway.length, ggoals: ggoalsAway1.get('ggoals') + ggoalsAway.length, games: ggoalsAway1.get('games') + 1}, options);
 
